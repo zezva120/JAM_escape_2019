3cc127d17c1ea4044d691fcdba9796b642871aa0
Samuel <besseausamuel@gmail.com>
2020-04-26 15:11:50 +0200

    Dilogue Modified

--- Assets/Nouveau dossier/Scripts/LightColorController.cs
+++ Assets/Nouveau dossier/Scripts/LightColorController.cs
@@ -10,7 +10,7 @@ public interface ColorSetterInterface
 [ExecuteInEditMode]
 public class LightColorController : MonoBehaviour
 {
-    [SerializeField] [Range(0,1)] float time;
+    [Range(0,1)] public float time;
 
     private ColorSetterInterface[] setters;
     private float currentTime = 0;
